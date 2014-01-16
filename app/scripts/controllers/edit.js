7d421116aad59b7747af95d6b965e7ba620ffde8
Scott Nath <github@scottnath.com>
2014-01-16 16:59:08 -0500

    date selector works

--- app/scripts/controllers/edit.js
+++ app/scripts/controllers/edit.js
@@ -3,12 +3,17 @@
 
 define([ 'angular' ], function() {
 
-  var editActivity = angular.module('ata.editActivity', ['firebase']);
+  var editActivity = angular.module('ata.editActivity', ['firebase','ui.date']);
 
   editActivity.controller('EditCtrl', function($scope, $location, $routeParams, $firebase, Firebase, fbURL, repeatOptions) {
     var projectUrl = fbURL + $routeParams.projectId;
     $scope.project = $firebase(new Firebase(projectUrl));
 	  $scope.repeatTypes = repeatOptions;
+	  $scope.dateOptions = {
+        changeYear: true,
+        changeMonth: true,
+        yearRange: '1900:-0'
+    };
     $scope.destroy = function() {
       $scope.project.$remove();
       $location.path('/');
